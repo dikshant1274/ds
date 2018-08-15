4b66213022fecc04338f0bd29fe5bb4bfc915651
Dikshant <dikshanta7852gmail.com>
2018-08-15 17:12:32 +0530

    postfix

--- stack2.cpp
+++ stack2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 struct node
 {
-  int data;
+  char data;
   node *next;
 };
 void push(node **root,int data)
@@ -62,13 +62,74 @@ void display(node *root)
     root=root->next;
   }
 }
+int prec(char a)
+{
+  switch (a) {
+    case '^':
+    {
+      return 3;
+    }
+    case '*':
+    case '/':
+    {
+      return 2;
+    }
+    case '+':
+    case '-':
+    {
+      return 1;
+    }
+  }
+}
 int main()
 {
-  node *root=NULL;
-  push(&root,10);
-  push(&root,20);
-  push(&root,30);
-  pop(&root);
-cout<<  top(&root)<<endl;
-  display(root);
+    string s,os;
+    cin>>s;
+    char c;
+    node *root=NULL;
+    int i;
+    int l=s.length();
+    for(i=0;i<l;i++)
+    {
+      if(s[i]>='a' && s[i]<='z')
+      {
+        os=os+s[i];
+      }
+      else if(s[i]>='A' && s[i]<='Z')
+      {
+        os=os+s[i];
+      }
+      else if(s[i]=='(')
+      {
+        push(&root,s[i]);
+      }
+      else if(s[i]==')')
+      {
+        while(top(&root)!='('&&(!isempty(root)))
+        {
+          c=pop(&root);
+          os=os+c;
+        }
+        if(top(&root)=='(')
+        {
+          pop(&root);
+        }
+      }
+      else
+      {
+        while(!isempty(root) && prec(s[i])<=prec(top(&root)))
+        {
+          c=pop(&root);
+          os=os+c;
+        }
+        push(&root,s[i]);
+      }
+    }
+    while(!isempty(root))
+    {
+      c=pop(&root);
+      os=os+c;
+    }
+
+    cout<<os<<endl;
 }
